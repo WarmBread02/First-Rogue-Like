0619451452eb185b8535602c3f5019cc0ad32c62
WarmBread02 <warmbread02@gmail.com>
2022-05-20 18:37:51 -0500

    massive overhaul: multiple floors, equipment, exp

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ def main() -> None:
         screen_width,
         screen_height,
         tileset=tileset,
-        title="First Roguelike!",
+        title="Yet Another Roguelike Tutorial",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
